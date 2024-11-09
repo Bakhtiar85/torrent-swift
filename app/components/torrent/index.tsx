9bc0815176d1424072aca15dbd9b2fbc47f4c07f
Bakhtiar85 <makhdoomabubakar85@gmail.com>
2024-11-10 00:47:43 +0500

    stream is downloading on the user end. UI/UX updated.

--- app/components/torrent/index.tsx
+++ app/components/torrent/index.tsx
@@ -66,30 +66,11 @@ const TorrentDownloader = () => {
         setShowProgress(true);
         setIsProgressButtonDisabled(0);
 
-        const randomTimeout = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
-        setIsProgressButtonDisabled(randomTimeout);
-
-        const countdownInterval = setInterval(() => {
-            setIsProgressButtonDisabled((prev) => {
-                if (prev !== null && prev > 1) {
-                    return prev - 1;
-                } else {
-                    clearInterval(countdownInterval);
-                    return null;
-                }
-            });
-        }, 1000);
+        let randomTimeout = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
 
         try {
             const response = await fetch(`/api/torrent/stream?taskId=${taskId}`);
             const data = await response.json();
-
-            if (!data.ok) {
-                alert("Download interupted. Reseting everything!");
-
-                progressResetCancel();
-                return;
-            }
 
             if (!isNaN(data.progress)) {
                 setProgress(data.progress);
@@ -99,10 +80,24 @@ const TorrentDownloader = () => {
             if (data.progress === 100) {
                 // Fetch the files available for download
                 setFiles(data.files || []); // Update files available for download
-                console.log("DATA: ", data.files)
+                console.log("DATA: ", data.files);
+                randomTimeout = 2;
             }
         } catch (error) {
             console.error('Error fetching progress:', error);
+        } finally {
+            setIsProgressButtonDisabled(randomTimeout);
+
+            const countdownInterval = setInterval(() => {
+                setIsProgressButtonDisabled((prev) => {
+                    if (prev !== null && prev > 1) {
+                        return prev - 1;
+                    } else {
+                        clearInterval(countdownInterval);
+                        return null;
+                    }
+                });
+            }, 1000);
         }
     };
 
@@ -148,7 +143,7 @@ const TorrentDownloader = () => {
             if (!response.ok) {
                 // Handle error if cancellation was unsuccessful
                 console.error('Failed to cancel task. Try in new tab or browser.');
-                return;
+                // return;
             }
 
             const data = await response.json();
@@ -160,6 +155,7 @@ const TorrentDownloader = () => {
             setTaskId(null);
             setFiles([]);
             setShowProgress(false);
+            setProgress(0);
             setIsProgressButtonDisabled(null);
 
             // Optionally, show a confirmation message to the user
@@ -171,30 +167,31 @@ const TorrentDownloader = () => {
     };
 
     return (
-        <div className="w-11/12 md:w-4/5 max-w-lg mx-auto -mt-28 p-6 bg-gray-800 rounded-lg shadow-xl text-white transition-all duration-300 ease-in-out hover:shadow-2xl">
+        <div className="w-11/12 md:w-4/5 max-w-lg mx-auto -mt-2 md:-mt-16 sm:-mt-28 p-4 sm:p-6 bg-gray-800 rounded-lg shadow-xl text-white transition-all duration-300 ease-in-out hover:shadow-2xl">
             {!taskId && !isUploading && (
                 <>
-                    <div className="mb-4">
-                        <label htmlFor="torrent-file" className="block text-base font-semibold text-gray-300 mb-2 tracking-wider shadow-md">
+                    <div className="mb-3 sm:mb-4">
+                        <label htmlFor="torrent-file" className="block text-sm lg:text-base font-semibold text-gray-300 mb-2 tracking-wider shadow-md">
                             Select Torrent File
                         </label>
                         <input
                             id="torrent-file"
                             type="file"
-                            className="w-full text-sm text-gray-300 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-600 hover:file:bg-blue-200"
+                            className="w-full text-xs lg:text-sm text-gray-300 file:mr-4 file:py-1.5 sm:file:py-2 file:px-3 sm:file:px-4 file:rounded-md file:border-0 file:text-xs sm:file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-600 hover:file:bg-blue-200"
                             onChange={handleFileChange}
                             accept=".torrent"
+                            required={true}
                         />
                     </div>
 
                     {file && (
-                        <p className="text-sm text-gray-400 mb-4 shadow-sm">Selected file: {file.name}</p>
+                        <p className="text-xs lg:text-sm text-gray-400 mb-3 sm:mb-4 shadow-sm">Selected file: {file.name}</p>
                     )}
 
                     <button
                         onClick={handleUpload}
                         disabled={!file}
-                        className={`w-full py-3 px-4 border border-transparent rounded-lg shadow-lg text-sm font-medium text-white tracking-wider ${file ? 'bg-blue-600 hover:bg-blue-700' : 'bg-gray-500 cursor-not-allowed'
+                        className={`w-full py-2 sm:py-3 px-3 sm:px-4 border border-transparent rounded-lg shadow-lg text-xs lg:text-sm font-medium text-white tracking-wider ${file ? 'bg-blue-600 hover:bg-blue-700' : 'bg-gray-500 cursor-not-allowed'
                             } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400 transition-all duration-200`}
                     >
                         Upload Torrent
@@ -204,18 +201,18 @@ const TorrentDownloader = () => {
 
             {taskId && (
                 <div>
-                    <div className="flex justify-between items-center">
-                        <p className="text-base text-gray-400 mb-4 italic">Torrent is processing...</p>
-                        <span className="text-xs text-red-600 mb-4 italic font-semibold underline hover:scale-150 hover:text-white">Or wait, we will notify you when done!</span>
+                    <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                        <p className="text-sm sm:text-base text-gray-400 mb-2 sm:mb-4 italic">Torrent is processing...</p>
+                        <span className="text-xs text-red-600 mb-4 italic font-semibold underline hover:scale-110 sm:hover:scale-150 hover:text-white hidden sm:block">Kindly wait, you will be notify when done!</span>
                     </div>
                     <button
                         onClick={handleProgressCheck}
                         disabled={isProgressButtonDisabled !== null}
-                        className={`relative w-full h-10 py-2 px-4 border border-transparent rounded-lg shadow-lg text-sm font-medium text-white ${isProgressButtonDisabled !== null ? 'bg-green-400/75 cursor-not-allowed' : 'bg-green-600 hover:bg-green-700'
+                        className={`relative w-full h-8 sm:h-10 py-1.5 sm:py-2 px-3 sm:px-4 border border-transparent rounded-lg shadow-lg text-xs sm:text-sm font-medium text-white ${isProgressButtonDisabled !== null ? 'bg-green-400/75 cursor-not-allowed' : 'bg-green-600 hover:bg-green-700'
                             } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 transition-all duration-200`}
                     >
                         {isProgressButtonDisabled !== null ? (
-                            <span className="absolute left-0 right-0 top-3 text-xs text-black">
+                            <span className="absolute left-0 right-0 top-2 sm:top-3 text-xs text-black">
                                 wait {isProgressButtonDisabled}s please!
                             </span>
                         ) : (
@@ -223,7 +220,7 @@ const TorrentDownloader = () => {
                         )}
                     </button>
 
-                    <p className='w-full text-center mt-5 text-red-600 mb-4 font-semibold underline hover:scale-150 hover:text-white'>
+                    <p className='w-full text-center mt-4 sm:mt-5 text-red-600 mb-3 sm:mb-4 text-xs md:text-sm md:font-semibold underline hover:scale-110 sm:hover:scale-150 hover:text-white'>
                         <strong>Warning!</strong>
                         <br />
                         <span>Frequently Clicking *Show Progress* Button can cause trouble. kindly wait...</span>
@@ -232,7 +229,7 @@ const TorrentDownloader = () => {
                     <button
                         onClick={progressResetCancel}
                         disabled={isProgressButtonDisabled !== null}
-                        className={`relative w-full h-10 py-2 px-4 border border-transparent rounded-lg shadow-lg text-sm font-medium text-white ${isProgressButtonDisabled !== null ? 'bg-blue-400/75 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700'
+                        className={`relative w-full h-8 sm:h-10 py-1.5 sm:py-2 px-3 sm:px-4 border border-transparent rounded-lg shadow-lg text-[10px] md:text-sm font-medium text-white ${isProgressButtonDisabled !== null ? 'bg-blue-400/75 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700'
                             } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400 transition-all duration-200`}
                     >
                         Re-Upload files/Terminate Download in Progress.
@@ -241,34 +238,51 @@ const TorrentDownloader = () => {
             )}
 
             {progress > 0 && (
-                <div className="mt-6">
-                    <h3 className="text-lg font-medium text-gray-300 mb-2 tracking-widest shadow-md">Download Progress</h3>
-                    <div className="w-full bg-gray-700 rounded-full h-2.5">
+                <div className="mt-4 sm:mt-6">
+                    <h3 className="text-base lg:text-lg font-medium text-gray-300 mb-2 tracking-widest shadow-md">Download Progress</h3>
+                    <div className="w-full bg-gray-700 rounded-full h-2 sm:h-2.5">
                         <div
-                            className="bg-blue-600 h-2.5 rounded-full"
+                            className="bg-blue-600 h-2 sm:h-2.5 rounded-full"
                             style={{ width: `${progress}%` }}
                         ></div>
                     </div>
-                    <p className="text-sm font-medium text-gray-400 mt-2 shadow-sm">{progress}% Complete</p>
+                    <p className="text-xs lg:text-sm font-medium text-gray-400 mt-2 shadow-sm">{progress.toFixed(2)}% Complete</p>
                 </div>
             )}
 
             {files.length > 0 && (
-                <div className="mt-6">
-                    <h3 className="text-lg font-medium text-gray-300 mb-4 tracking-wide shadow-md">Files Available for Download</h3>
-                    <ul className="space-y-3">
-                        {files?.map(({ name, length, downloaded, progress, path, mime, streamReady }, index) => (
-                            <li key={index} className="flex items-center justify-between py-3 px-4 bg-gray-700 rounded-lg shadow-sm">
-                                <span className="text-sm font-medium text-gray-300 shadow-sm">{name}</span>
-                                <button
-                                    onClick={() => handleFileDownload(index)}
-                                    className="ml-4 inline-flex items-center px-3 py-1 border border-transparent text-xs font-medium rounded-md shadow-lg text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition-all duration-200"
-                                >
-                                    Download
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+                <div className="mt-4 sm:mt-6">
+                    <h3 className="text-base lg:text-lg font-medium text-gray-300 mb-3 sm:mb-4 tracking-wide shadow-md">Files Available for Download</h3>
+                    <div className="max-h-48 md:max-h-[300px] overflow-y-auto pr-2 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800">
+                        <ul className="space-y-2 sm:space-y-3">
+                            {files?.map(({ name, length, downloaded, progress, path, mime, streamReady }, index) => (
+                                <li key={index} className="flex items-center justify-between py-1.5 sm:py-3 px-1 sm:px-4 bg-gray-700 rounded-lg shadow-sm">
+                                    <span className="text-[10px] lg:text-sm italic font-medium text-gray-300">{index++}: &nbsp;</span>
+                                    <span className="text-[10px] lg:text-sm font-medium text-gray-300 shadow-sm truncate max-w-[30%]">{name}</span>
+
+                                    {progress > 0 && (
+                                        <div className="flex-1 w-3/5 mx-2">
+                                            <div className="w-full bg-gray-700 rounded-full h-2 sm:h-2.5 relative">
+                                                <div
+                                                    className="bg-blue-600 h-2 sm:h-2.5 rounded-full"
+                                                    style={{ width: `${progress}%` }}
+                                                ></div>
+                                                <p className="text-[8px] lg:text-sm font-medium text-gray-400 mt-0 shadow-sm absolute left-0 right-0 text-center">{progress.toFixed(2)}%</p>
+                                            </div>
+                                        </div>
+                                    )}
+
+                                    {progress && <button
+                                        disabled={progress !== 100}
+                                        onClick={() => handleFileDownload(index)}
+                                        className={`ml-2 sm:ml-4 inline-flex items-center px-2 sm:px-3 py-1 border border-transparent text-[10px] md:text-xs font-medium rounded-md shadow-lg text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition-all duration-200 disabled:opacity-0`}
+                                    >
+                                        Download
+                                    </button>}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             )}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,21 @@ import HomePage from './components/torrent'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-10 bg-gray-900 text-white">
-      <div className="z-10 w-full items-center justify-center text-center font-serif text-3xl p-5 tracking-wider">
-        <h1 className="font-extrabold text-4xl md:text-6xl text-blue-500 drop-shadow-lg shadow-blue-500">
+    <main className="flex min-h-screen flex-col items-center justify-between p-3 sm:p-4 md:p-10 bg-gray-900 text-white">
+      <div className="z-10 w-full text-center font-serif p-3 sm:p-5 tracking-wider">
+        <h1 className="font-extrabold text-3xl sm:text-4xl xl:text-6xl text-blue-500 drop-shadow-lg shadow-blue-500 sm:mb-0">
           DarkTorrent Hub
         </h1>
-        <p className="text-xl md:text-3xl text-gray-300 mt-4 italic shadow-md">
+        <p className="text-lg sm:text-xl xl:text-3xl text-gray-300 mt-0.5 xl:mt-3 italic shadow-md">
           Secure & Anonymous Downloads
         </p>
       </div>
 
-      <div className="flex-grow w-full flex flex-col justify-center items-center">
+      <div className="flex-grow w-full flex flex-col justify-center items-center my-4 sm:my-6">
         <HomePage />
       </div>
 
-      <footer className="w-full text-lg md:text-xl text-center p-6 text-gray-400 shadow-md">
+      <footer className="w-full text-sm xl:text-lg text-center p-4 sm:p-6 text-gray-400 shadow-md">
         &copy; {new Date().getFullYear()} DarkTorrent Hub - Fast, Secure, Anonymous Downloads
       </footer>
     </main>
